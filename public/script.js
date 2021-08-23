60590836c506a0b170a349a96408dcb6af321dc5
stephanieraymos <stephanieraymos@gmail.com>
2021-08-22 20:42:10 -0700

    Peer instance working

--- public/script.js
+++ public/script.js
@@ -1,7 +1,10 @@
 const socket = io("/");
-
+const myPeer = new Peer(undefined, {
+  host: "/",
+  port: '3001'
+});
 socket.emit("join-room", ROOM_ID, 10);
 
-socket.on("user-connected", userId => {
-    console.log('User connected: ' + userId);
-})
+socket.on("user-connected", (userId) => {
+  console.log("User connected: " + userId);
+});
