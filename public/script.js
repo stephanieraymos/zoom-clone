8d9118bd18e7df116199bac200c9b5d741aa96ff
stephanieraymos <stephanieraymos@gmail.com>
2021-08-22 21:01:52 -0700

    Video working

--- public/script.js
+++ public/script.js
@@ -1,10 +1,34 @@
 const socket = io("/");
+const videoGrid = document.getElementById("video-grid");
+
 const myPeer = new Peer(undefined, {
   host: "/",
-  port: '3001'
+  port: "3001",
 });
-socket.emit("join-room", ROOM_ID, 10);
+const myVideo = document.createElement("video");
+myVideo.nuted = true; // Mutes ourselves so we don't hear our own audio
+
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    addVideoStream(myVideo, stream);
+  });
+myPeer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
 
 socket.on("user-connected", (userId) => {
   console.log("User connected: " + userId);
 });
+
+// Once video is loaded on page, play the video
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+}
